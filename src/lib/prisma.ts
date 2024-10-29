c61f9c6eac94e3519ce3fbf8a727632738213f3f
Heri Rusmanto <retphern@gmail.com>
2024-10-29 09:38:14 +0700

    fix(prisma): global

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,21 +6,12 @@ if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
   // Extend the NodeJS.Global interface to include the prisma property
-  interface CustomNodeJsGlobal extends NodeJS.Global {
-    prisma: PrismaClient;
-  }
-
-  const globalForPrisma = global as CustomNodeJsGlobal;
+  const globalForPrisma = global as typeof global & { prisma?: PrismaClient };
 
   if (!globalForPrisma.prisma) {
     globalForPrisma.prisma = new PrismaClient();
   }
   prisma = globalForPrisma.prisma;
-}
-
-// Declare the global prisma variable
-declare global {
-  const prisma: PrismaClient | undefined;
 }
 
 export default prisma;
