b88e91985c6adfd6b59203dcb823d0abbdc85727
hveda <retphern@gmail.com>
2025-10-04 01:58:42 +0900

    feat: validates environment var
    Adds validation of environment variables using Zod schema.
    Logs errors and throws an exception if validation fails,
    ensuring that the application only starts with valid configurations.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,11 +6,28 @@
  */
 import { z } from 'zod';
 
+import logger from '@/lib/logger';
+
 const envVariables = z.object({
+  // Public variables
   NEXT_PUBLIC_SHOW_LOGGER: z.enum(['true', 'false']).optional(),
+  NEXT_PUBLIC_VERCEL_ANALYTICS_ID: z.string().optional(),
+
+  // Server-side variables
+  POSTGRES_PRISMA_URL: z.string().url(),
+  POSTGRES_URL_NON_POOLING: z.string().url(),
+  ADMIN_PASSWORD: z.string().min(1, { message: 'ADMIN_PASSWORD is required.' }),
+  AUTH_SECRET: z
+    .string()
+    .min(32, { message: 'AUTH_SECRET must be at least 32 characters.' }),
 });
 
-envVariables.parse(process.env);
+try {
+  envVariables.parse(process.env);
+} catch (error) {
+  logger(error, '❌ Invalid environment variables');
+  throw new Error('Invalid environment variables');
+}
 
 declare global {
   namespace NodeJS {
